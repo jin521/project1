3514e9d4a2f4b7a8161b5765c333d62ed8e84893
jin521 <jinlc220@126.com>
2016-07-14 11:56:16 +1000

    remove grocoder from model

--- app/models/story.rb
+++ app/models/story.rb
@@ -13,7 +13,5 @@
 class Story < ActiveRecord::Base
     belongs_to :user
     has_many :likes
-    geocoded_by :ip_address, :latitude => :lat, :longitude => :lon
-    reverse_geocoded_by :latitude, :longitude, :address => :location
-    after_validation :geocode, :reverse_geocode
+
 end
